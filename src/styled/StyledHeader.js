4478e4bf4be65335c04988952aa432831d549a35
lofisoul <jason.nolfi@gmail.com>
2020-05-31 23:14:37 -0500

    Changed header padding

--- src/styled/StyledHeader.js
+++ src/styled/StyledHeader.js
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
 
 const StyledHeader = styled.header`
-	padding: 0 3rem;
+	padding: 0 1rem;
 	background-color: #fff;
 	box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.07);
 	display: flex;
 	justify-content: space-between;
 	align-content: center;
 	height: 70px;
+	@media (min-width: 576px) {
+		padding: 0 3rem;
+	}
 	& h1 {
 		margin: 0;
 		background-image: url(favicon.png);
