80ff7070075cf7cd6dc5c5073ac408f471798c49
Jason Nolfi <jason.nolfi@gmail.com>
2022-11-22 12:49:46 -0500

    refactor: Code cleanup add env vars

--- src/hooks/useSoundcloud.js
+++ src/hooks/useSoundcloud.js
@@ -4,14 +4,11 @@ import {useEffect} from 'react';
 //create an expire time to check refresh token
 
 export function useSoundcloud() {
-	const tokenURL = `https://api.soundcloud.com/oauth2/token?grant_type=client_credentials&client_id=41aceb0d516e657897a0eb7ab22c9f99&client_secret=3637e5ba852bf8d02a7714d22e486854`;
-	const refreshURL = `https://api.soundcloud.com/oauth2/token?grant_type=refresh_token&client_id=41aceb0d516e657897a0eb7ab22c9f99&client_secret=3637e5ba852bf8d02a7714d22e486854&refresh_token=`;
+	const tokenURL = `https://api.soundcloud.com/oauth2/token?grant_type=client_credentials&client_id=${process.env.REACT_APP_SC_ID}&client_secret=${process.env.REACT_APP_SC_SECRET}`;
+	const refreshURL = `https://api.soundcloud.com/oauth2/token?grant_type=refresh_token&client_id=${process.env.REACT_APP_SC_ID}&client_secret=${process.env.REACT_APP_SC_SECRET}&refresh_token=`;
 
 	useEffect(() => {
-		console.log('USER');
-		console.log(localStorage);
 		if (!localStorage.accessToken) {
-			console.log('fetch');
 			fetch(tokenURL, {
 				headers: {
 					Accept: 'application/json; charset=utf-8',
@@ -21,7 +18,6 @@ export function useSoundcloud() {
 			})
 				.then(res => res.json())
 				.then(data => {
-					console.log(data);
 					const now = new Date();
 					const tokenExpire = new Date(
 						now.getTime() + data.expires_in * 1000,
@@ -30,6 +26,33 @@ export function useSoundcloud() {
 					localStorage.setItem('refreshToken', data.refresh_token);
 					localStorage.setItem('expires', tokenExpire);
 				});
+		}
+
+		if (localStorage.accessToken) {
+			const now = new Date();
+
+			if (localStorage.expires < now) {
+				fetch(`${refreshURL}${localStorage.refreshToken}`, {
+					headers: {
+						Accept: 'application/json; charset=utf-8',
+						'Content-Type': 'application/x-www-form-urlencoded',
+					},
+					method: 'POST',
+				})
+					.then(res => res.json())
+					.then(data => {
+						const now = new Date();
+						const tokenExpire = new Date(
+							now.getTime() + data.expires_in * 1000,
+						);
+						localStorage.setItem('accessToken', data.access_token);
+						localStorage.setItem(
+							'refreshToken',
+							data.refresh_token,
+						);
+						localStorage.setItem('expires', tokenExpire);
+					});
+			}
 		}
 	}, [tokenURL, refreshURL]);
 }
