8861e2f942bb7b8031f1e72772ee3530b3d387e4
Jason Nolfi <jason.nolfi@gmail.com>
2022-11-30 16:59:10 -0500

    refactor: Track outline fix and player transition

--- src/styled/StyledList.js
+++ src/styled/StyledList.js
@@ -31,7 +31,7 @@ const StyledList = styled.ul`
 		transition: 0.3s;
 
 		&.active {
-			outline: 3px solid #ffdaab;
+			box-shadow: 0px 0px 0px 3px #ffdaab, 5px 5px 0px 1px rgba(0, 0, 0, 50%);
 			
 			.user-img {
 				background: #ffdaab;
--- src/styled/StyledPlayer.js
+++ src/styled/StyledPlayer.js
@@ -4,17 +4,18 @@ const StyledPlayer = styled.div`
 	position: fixed;
 	bottom: 0;
 	background: #fff;
-	height: ${props => (props.currentTrack ? '70px' : '0')};
+	transform: ${props =>
+		props.currentTrack ? 'translateY(0)' : 'translateY(100%)'};
 	width: 100%;
 	left: 0;
-	transition: height 0.2s cubic-bezier(0, 0.42, 0.76, 1.27);
+	transition: transform 0.2s cubic-bezier(0, 0.42, 0.76, 1.27);
 	z-index: 5;
 	& > div {
 		max-width: 1600px;
 		margin: 0 auto;
 		display: flex;
 		align-items: center;
-		padding: 5px 5px calc(5px + env(safe-area-inset-bottom));
+		padding: 15px 15px calc(15px + env(safe-area-inset-bottom));
 		height: 100%;
 		border-top: 3px solid #afe3d3;
 		& > div {
